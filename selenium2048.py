2c2a6a8ce502e7ee436a9a7bb993921077c488be
aemann2 <aemann2@gmail.com>
2022-06-13 15:45:41 -0700

    added sysargs, infinite looping, round stats

--- selenium2048.py
+++ selenium2048.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import sys
+from itertools import count
 import random
 
 class GameBot:
@@ -9,15 +11,20 @@ class GameBot:
     def __init__(self):
         self.high_score = 0
         self.game_over = False
-        self.driver = webdriver.Firefox()
+        try:
+            self.rounds = int(sys.argv[1])
+        except:
+            self.rounds = None
+        self.driver = webdriver.Chrome()
+        self.roundsCompleted = 0
         self.driver.get('https://play2048.co/')
         self.container = self.driver.find_element(by=By.TAG_NAME, value='html')
 
-    def start_round(self):
+    def _start_round(self):
         start = self.driver.find_element(by=By.CLASS_NAME, value='restart-button')
         start.click()
     
-    def key_press_loop(self):
+    def _key_press_loop(self):
         self.container.send_keys(Keys.ARROW_RIGHT)
         # sleep(.1)
         self.container.send_keys(Keys.ARROW_DOWN)
@@ -27,7 +34,7 @@ class GameBot:
         self.container.send_keys(Keys.ARROW_UP)
         # sleep(.1)
 
-    def check_for_game_over(self):
+    def _check_for_game_over(self):
             self.driver.find_element(by=By.CLASS_NAME, value='game-over')
             game_over = True
             # set high score to best score
@@ -39,20 +46,27 @@ class GameBot:
             return game_over, high_score
 
     def game_loop(self):
-        for i in range(4):
-            self.game_over = False
-            self.start_round()
-            while self.game_over == False:
-                # TODO: randomize key entries
-                self.key_press_loop()
-            # TODO: try to find a different way to do this rather than a try/catch
-                try: 
-                    self.game_over, self.high_score = self.check_for_game_over()
-                except:
-                    continue
+        if self.rounds:
+            for i in range(self.rounds):
+                self._round_loop()
+                
+        else:
+            for i in count(0):
+                self._round_loop()
+    
+    def _round_loop(self):
+        self.game_over = False
+        self._start_round()
+        while self.game_over == False:
+            # TODO: randomize key entries
+            self._key_press_loop()
+            try: 
+                self.game_over, self.high_score = self._check_for_game_over()
+            except:
+                continue
+        self.roundsCompleted += 1
 
 game_bot = GameBot()
-
 game_bot.game_loop()
 
-print(f'Best high score: {game_bot.high_score}')
+print(f'Best high score over {game_bot.roundsCompleted} rounds: {game_bot.high_score}')
