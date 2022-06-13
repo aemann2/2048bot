65e146b6a53565501b39fd5f179fad2e52e3bbfd
aemann2 <aemann2@gmail.com>
2022-06-12 19:42:57 -0700

    functionize game loop, added brief sleep()

--- selenium2048.py
+++ selenium2048.py
@@ -7,7 +7,8 @@ from time import sleep
 driver = webdriver.Firefox()
 driver.get('https://play2048.co/')
 
-# Firefox will wait until the page is completely loaded (including ads) before starting
+# Firefox will wait until the page is completely loaded (including ads) before starting. 
+# TODO: Might consider switching to Chrome()?
 sleep(3)
 
 container = driver.find_element(by=By.TAG_NAME, value='html')
@@ -20,28 +21,42 @@ high_score = 0
 # - Display some kind of stats at the end?
 # - See if you can get a browser alert to display at the end
 
-# TODO: remove hard-coded cycle and accept input prompt
-for i in range(4):
-    game_over = False
-    # TODO: functionize start cycle
+
+def start_round():
     start = driver.find_element(by=By.CLASS_NAME, value='restart-button')
     start.click()
-    while game_over == False:
-        # TODO: functionize (and maybe randomize) key entries. consider adding a small sleep between each key press
-        container.send_keys(Keys.ARROW_RIGHT)
-        container.send_keys(Keys.ARROW_DOWN)
-        container.send_keys(Keys.ARROW_LEFT)
-        container.send_keys(Keys.ARROW_UP)
-    # TODO: functionize game-over loop
-    # TODO: try to find a different way to do this rather than a try/catch
-        try: 
+
+def key_press_loop():
+    container.send_keys(Keys.ARROW_RIGHT)
+    # sleep(.1)
+    container.send_keys(Keys.ARROW_DOWN)
+    # sleep(.1)
+    container.send_keys(Keys.ARROW_LEFT)
+    # sleep(.1)
+    container.send_keys(Keys.ARROW_UP)
+    # sleep(.1)
+
+def check_for_game_over(game_over, high_score):
             driver.find_element(by=By.CLASS_NAME, value='game-over')
             game_over = True
             # set high score to best score
             best_score_value = driver.find_element(by=By.CLASS_NAME, value='best-container')
             high_score = best_score_value.get_attribute('innerHTML')
+            # restart round
             retry_button = driver.find_element(by=By.CLASS_NAME, value='retry-button')
             retry_button.click()
+            return game_over, high_score
+
+# TODO: remove hard-coded cycle and accept input prompt
+for i in range(4):
+    game_over = False
+    start_round()
+    while game_over == False:
+        # TODO: randomize key entries
+        key_press_loop()
+    # TODO: try to find a different way to do this rather than a try/catch
+        try: 
+            game_over, high_score = check_for_game_over(game_over, high_score)
         except:
             continue
 
